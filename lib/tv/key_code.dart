12aa8fcb97520e10c987b27f0c4cdb1a88b12b6d
Vadim <44135514+vodemn@users.noreply.github.com>
2020-01-17 12:01:34 +0300

    Create key_code.dart

---
+++ lib/tv/key_code.dart
@@ -0,0 +1,23 @@
+// Common
+const int KEY_UP = 19;
+const int KEY_DOWN = 20;
+const int KEY_LEFT = 21;
+const int KEY_RIGHT = 22;
+
+// Remote controls
+const int KEY_CENTER = 23;
+const int MENU = 82;
+const int INFO = 165;
+const int BACK = 4;
+const int PREVIOUS = 89;
+const int NEXT = 90;
+const int PAUSE = 85;
+
+// PC
+const int ENTER = 66;
+const int BACKSPACE = 67;
+const int INFO_KEY = 37;
+const int MENU_KEY = 41;
+
+// Additional
+const int FOCUS_ON_PROGRAMS = 0;
