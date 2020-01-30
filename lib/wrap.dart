92bf3ac2bbd564f4c22d5b18f04b10cc0d3c9401
Vadim <44135514+vodemn@users.noreply.github.com>
2020-01-30 14:44:07 +0300

    Update wrap.dart

--- lib/wrap.dart
+++ lib/wrap.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CustomWrap extends StatefulWidget {
+class CustomWrap extends StatelessWidget {
   final double width;
   final double itemWidth;
   final double horizontalPadding;
@@ -16,27 +16,17 @@ class CustomWrap extends StatefulWidget {
       this.verticalPadding,
       this.scrollController});
 
-  @override
-  _CustomWrapState createState() => _CustomWrapState();
-}
-
-class _CustomWrapState extends State<CustomWrap> {
   int _maxItemCount;
   List<List<Widget>> rows = [];
   double _vertPadding;
   double _horPadding;
 
   @override
-  void initState() {
-    super.initState();
-    _vertPadding = widget.verticalPadding ?? 0;
-    _horPadding = widget.horizontalPadding ?? 0;
-    _maxItemCount = widget.width ~/ (widget.itemWidth + _horPadding);
-    _splitRows();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    _vertPadding = verticalPadding ?? 0;
+    _horPadding = horizontalPadding ?? 0;
+    _maxItemCount = width ~/ (itemWidth + _horPadding);
+    _splitRows();
     return Padding(
       padding: EdgeInsets.symmetric(vertical: _vertPadding),
       child: ListView.separated(
@@ -53,10 +43,10 @@ class _CustomWrapState extends State<CustomWrap> {
 
   void _splitRows() {
     int i = 0;
-    for (i = 0; i < widget.children.length; i++) {
+    for (i = 0; i < children.length; i++) {
       List<Widget> row = [];
-      for (int k = 0; k < _maxItemCount && i < widget.children.length; k++) {
-        row.add(widget.children[i]);
+      for (int k = 0; k < _maxItemCount && i < children.length; k++) {
+        row.add(children[i]);
         row.add(SizedBox(width: _horPadding));
         i++;
       }
