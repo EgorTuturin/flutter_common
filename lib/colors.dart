1bb4513165c863e09c56f91ac2f11e119a5315f7
Vadim <44135514+vodemn@users.noreply.github.com>
2020-08-13 17:14:58 +0300

    Clean up (#10)
    * play\pause buttons
    
    * Colors fix
    
    * Primary color review
    
    * Time format
    
    * Update theming.dart
    
    * Revert "Update theming.dart"
    
    This reverts commit 8acc8c3376ca6636d28e635a3758d5b0ec15c7f8.
    
    * Update theming.dart
    
    * Primary color fix
    
    * Create draggable_scrollbar.dart
    
    * Update table.dart
    
    * Update draggable_scrollbar.dart
    
    * Update table.dart
    
    * Update theming.dart
    
    * Update runtime_device.dart
    
    * Clean up

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:persist_theme/persist_theme.dart';
 
 class CustomColor {
   CustomColor();
@@ -8,32 +7,10 @@ class CustomColor {
     return Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black;
   }
 
-  Color primaryColorBrightness(ThemeModel model, {Color dark, Color light}) {
-    if (ThemeData.estimateBrightnessForColor(model.primaryColor) == Brightness.light || model.type == ThemeType.light) {
-      return light ?? Colors.black;
-    }
-    return dark ?? Colors.white;
-  }
-
-  Color accentColorBrightness(ThemeModel model, {Color dark, Color light}) {
-    if (ThemeData.estimateBrightnessForColor(model.accentColor) == Brightness.light || model.type == ThemeType.light) {
-      return light ?? Colors.black;
-    }
-    return dark ?? Colors.white;
-  }
-
   Color backGroundColorBrightness(Color color, {Color dark, Color light}) {
     if (ThemeData.estimateBrightnessForColor(color) == Brightness.light) {
       return light ?? Colors.black;
     }
     return dark ?? Colors.white;
-  }
-
-  Color tvSelectedColor() {
-    return Colors.amber;
-  }
-
-  Color tvUnselectedColor() {
-    return Colors.grey;
   }
 }
