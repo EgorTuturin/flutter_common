488784bf1f09d35c9b3ba3de4bdf31c3417c6b42
topilski <atopilski@gmail.com>
2020-11-17 13:28:50 +0300

    Logos Review

--- lib/base/controls/logo.dart
+++ lib/base/controls/logo.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
-class Logo extends StatelessWidget {
+class CircleAssetLogo extends StatelessWidget {
   final String logoPath;
+  final double radius;
 
-  Logo(this.logoPath);
+  CircleAssetLogo(this.logoPath, {this.radius = 48.0});
 
   @override
   Widget build(BuildContext context) {
-    return CircleAvatar(backgroundColor: Colors.transparent, radius: 48.0, child: Image.asset(logoPath));
+    return CircleAvatar(backgroundColor: Colors.transparent, radius: radius, child: Image.asset(logoPath));
+  }
+}
+
+class CustomAssetLogo extends StatelessWidget {
+  final String logoPath;
+  final double width;
+  final double height;
+
+  CustomAssetLogo(this.logoPath, {this.width, this.height});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(logoPath, width: width, height: height);
   }
 }
